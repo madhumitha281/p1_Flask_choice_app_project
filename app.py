a3af75023381191e1ef99fec91e5d54ba4ed7bf0
madhumitha281 <vellamunjimadhumitha6@gmail.com>
2026-03-10 01:01:33 +0530

    app updated

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
+
+EMAIL = "vmnmitha@gmail.com"
+PASSWORD = "dehn zadj drbv thkn"
+
+
+def send_email(choice):
+    subject = "Choice Selected"
+    body = f"Someone selected: {choice}"
+
+    message = f"Subject:{subject}\n\n{body}"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(EMAIL, PASSWORD)
+        server.sendmail(EMAIL, EMAIL, message)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/choose", methods=["POST"])
 def choose():
     choice = request.form["choice"]
+
+    send_email(choice)
+
     return f"Thanks for choosing {choice} 😊"
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
